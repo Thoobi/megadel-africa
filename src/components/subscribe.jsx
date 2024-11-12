498a9e2e4b040948fbfdbd61fb80a96acbffea9f
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-12 10:08:39 +0100

    style: improve layout and styling of Subscribe component for better readability

--- src/components/subscribe.jsx
+++ src/components/subscribe.jsx
@@ -1,26 +1,23 @@
 export default function Subscribe() {
   return (
-    <section className="flex flex-col items-stretch text-center justify-center text-custom-blue-500 p-[3rem] gap-[1rem] sm:p-[4rem] lg:items-center xl:p-[8rem]">
-      <h2 className="font-medium text-[3rem]">
+    <section className="flex flex-col items-stretch text-center justify-center text-custom-blue-500 p-12 gap-4 sm:p-16 lg:items-center xl:p-32">
+      <h2 className="font-medium text-32">
         Stay up to date with Megadel Africa
       </h2>
-      <p className="font-medium lg:mb-[2rem] text-[1.8rem]">
+      <p className="font-medium lg:mb-8 text-18">
         Subscribe to our newsletter for latest news, projects and updates
       </p>
-      <form
-        action=""
-        method="POST"
-        className="flex flex-col items-stretch rounded-xl gap-[2rem] mt-[1rem] lg:mt-0 lg:flex-row lg:border-[2px]"
-      >
+      <form className="flex flex-col items-stretch rounded-xl gap-8 mt-4 w-2/6 lg:gap-0 lg:mt-0 lg:flex-row lg:border-2">
         <input
           type="text"
           name="text"
           placeholder="We saved a nice spot for your email"
-          className="py-[1.5rem] px-[3rem] border-[2px] lg:border-0"
+          className="py-6 px-6 border-2 outline-none flex-1 text-16 lg:border-0"
+          required
         />
         <button
           type="submit"
-          className="bg-custom-blue-100 text-white rounded-xl py-[1.5rem] px-[3rem]"
+          className="bg-custom-blue-100 text-white rounded-xl py-6 px-12 text-16"
         >
           Subscribe
         </button>
