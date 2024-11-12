1a00f21bc542464b262a5b3504c03d3ac62feede
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-12 19:23:48 +0100

    feat: add Contact component with header and mission statement

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <>
+      <header>
+        <h1>Empowering Global Innovation</h1>
+        <p>Our Mission: To Fuel African Entrepreneurship</p>
+      </header>
+    </>
+  );
+}
