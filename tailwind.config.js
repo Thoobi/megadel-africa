b99f00b439b2388021054a483d36b17319971667
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-13 13:09:50 +0100

    feat: update custom radial gradient in Tailwind CSS configuration for enhanced visual effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ export default {
         },
       },
       backgroundImage: {
-        "custom-radial": "radial-gradient(circle, #0A3A86, #09295D)",
+        "custom-radial": "radial-gradient(circle, #0A3A86 60%, #09295D)",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
