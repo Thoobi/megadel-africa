282905aa403728233e2c70fc9dfcb3024fb076c6
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-13 07:25:13 +0100

    feat: update Button component to accept classname prop for customizable styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
 import PropTypes from "prop-types";
 
-export default function Button({ children }) {
-  return <button>{children}</button>;
+export default function Button({ classname, children }) {
+  return (
+    <button
+      className={`bg-custom-blue-100 text-white text-16 py-3 lg:py-6 lg:px-12 ${classname}`}
+    >
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
+  classname: PropTypes.string.isRequired,
 };
