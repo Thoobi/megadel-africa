1ac0843cb6b3075c5bd6cd77c89092166bf0b878
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-21 19:05:19 +0100

    feat: implement FAQ section with dynamic content rendering

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,4 +1,6 @@
+import { FaPlus } from "react-icons/fa";
 import Section from "../components/Section";
+import { faqs } from "../utils/util";
 
 export default function Faq() {
   return (
@@ -11,6 +13,20 @@ export default function Faq() {
           <p className="text-18 sm:text-24 font-light">
             Got a question? Someone&apos;s probably asked it before.
           </p>
+        </Section>
+
+        <Section classname="flex-col gap-8">
+          {faqs.map((faq, i) => (
+            <div
+              key={i}
+              className="flex items-center justify-between w-full bg-gray-300 px-4"
+            >
+              {faq.title}
+              <span>
+                <FaPlus />
+              </span>
+            </div>
+          ))}
         </Section>
       </main>
     </>
