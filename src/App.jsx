9c65a8df5ec97e7bfabed09ac382755ebbe9f910
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-21 17:40:06 +0100

    feat: add FAQ route to application for improved user navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Homepage";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Faq from "./pages/Faq";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/faqs" element={<Faq />} />
       </Routes>
     </BrowserRouter>
   );
